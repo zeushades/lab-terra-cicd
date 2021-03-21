a78322201e258571c28516ff00f6ecb3aed726ff
anhbt-2176 <bui.tuan.anh-b@sun-asterisk.com>
2021-03-21 23:30:01 +0700

    update main.tf

--- main.tf
+++ main.tf
@@ -30,8 +30,7 @@ resource "aws_instance" "example" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
+  
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
@@ -47,15 +46,15 @@ resource "aws_instance" "example" {
 # CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+# resource "aws_security_group" "instance" {
+#   name = "terraform-example-instance"
 
-  # Inbound HTTP from anywhere
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   # Inbound HTTP from anywhere
+#   ingress {
+#     from_port   = var.server_port
+#     to_port     = var.server_port
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
